8ffb9585e4453460350397a8ed7d01066b4fcf4f
rikicavaz <120377343+Rikicavaz77@users.noreply.github.com>
2024-11-16 17:10:29 +0100

    Feature utils (#7)
    * add aot chall
    
    * add utils

---
+++ Crypto/utils/caesar_cipher.py
@@ -0,0 +1,27 @@
+def ceaser_cipher_encoder(plain_text, shift):
+    result = ''
+    for c in plain_text:
+        if c.isalpha():     # check if the character is a letter
+            offset = 65 if c.isupper() else 97
+            # - offset: normalizes the position of alphabetic characters in the range 0-25
+            result += chr((ord(c) - offset + shift) % 26 + offset)
+        else:
+            result += c
+    return result
+
+def ceaser_cipher_decoder(cipher_text, shift):
+    result = ''
+    for c in cipher_text:
+        if c.isalpha():     # check if the character is a letter
+            offset = 65 if c.isupper() else 97
+            # - offset: normalizes the position of alphabetic characters in the range 0-25
+            result += chr((ord(c) - offset - shift) % 26 + offset)
+        else:
+            result += c
+    return result
+
+input = "QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD"
+
+for i in range(1, 26):
+    decoded = ceaser_cipher_decoder(input, i)
+    print(f"Step={i}\t{decoded}")
